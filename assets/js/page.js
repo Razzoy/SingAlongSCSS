f3d4967a6c35d946ab048e552a6302e24d2838ab
Razzoy <1081892@ucn.dk>
2024-06-09 16:35:42 +0200

    work update
    Dato: 09-06-2024

---
+++ assets/js/page.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.getElementById('burgerMenu');
+    const burgerLinks = document.getElementById('burgerLinks');
+
+    burgerMenu.addEventListener('click', () => {
+        burgerLinks.classList.toggle('active');
+    });
+});
